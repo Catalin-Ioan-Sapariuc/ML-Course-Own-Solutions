217ba346496a46cd5e0cc328bd7b857e81c55dc3
Catalin-Ioan-Sapariuc <60756653+Catalin-Ioan-Sapariuc@users.noreply.github.com>
2021-02-25 23:16:02 +0200

    Add files via upload

---
+++ projectData.m
@@ -0,0 +1,32 @@
+function Z = projectData(X, U, K)
+%PROJECTDATA Computes the reduced data representation when projecting only 
+%on to the top k eigenvectors
+%   Z = projectData(X, U, K) computes the projection of 
+%   the normalized inputs X into the reduced dimensional space spanned by
+%   the first K columns of U. It returns the projected examples in Z.
+%
+
+% You need to return the following variables correctly.
+%Z = zeros(size(X, 1), K);
+Ureduced = U(:, 1:K);
+
+Z = X * Ureduced; 
+
+%% X is of size m X n , Ureduced is of size n X K,  
+%% therefore Z is of size m X K (that is, the reduced examples are the
+%% rows of Z) 
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the projection of the data using only the top K 
+%               eigenvectors in U (first K columns). 
+%               For the i-th example X(i,:), the projection on to the k-th 
+%               eigenvector is given as follows:
+%                    x = X(i, :)';
+%                    projection_k = x' * U(:, k);
+%
+
+
+
+
+% =============================================================
+
+end
